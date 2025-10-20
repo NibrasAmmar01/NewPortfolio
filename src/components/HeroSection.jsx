30a87bc1441865aa97b4226c343db6ffaa69d633
Nibras Ammar <nibras.ammar@etudiant-enit.utm.tn>
2025-10-20 19:49:31 +0100

    Setting Up Hero Section

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,24 @@
+export const HeroSection = () => {
+    return <section id="hero" className="relative min-h-screen flex flex-col items-center justify-center px-4">
+        <div className="container max-w-4xl mx-auto text-center z-10">
+            <div className="space-y-6">
+                <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
+                    <span className="opacity-0 animate-fade-in">Hi, I'm</span>
+                    <span className="text-primary opacity-0 animate-fade-in-delay-1"> Nebras</span>
+                    <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2"> Ammar</span>
+                </h1>
+
+                <p className="text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">
+                    I develop web applications, user interfaces, AI solutions, and software systems <br className='sm:block hidden' /> to build practical and efficient solutions.
+                </p>
+
+                <div className="opacity-0 animate-fade-in-delay-4">
+                    <a href="#projects" className="cosmic-button">
+                        View My Work
+                    </a>
+                </div>
+            </div>
+
+        </div>
+    </section>
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "../components/StarBackground";
 import { Navbar } from "../components/Navbar";
+import { HeroSection } from "../components/HeroSection";
 export const Home = () => {
     return ( <div className="min-h-screen bg-background text-foreground overflow-x-hidden ">
         {/* Theme Toggle*/ }
@@ -13,6 +14,9 @@ export const Home = () => {
         <Navbar />
 
         {/* Main Contact */}
+        <main>
+            <HeroSection></HeroSection>
+        </main>
 
         {/* Fouter */}
 
